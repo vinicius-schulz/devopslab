b2fa91169740510bc0b6a8bbacdb4c7d3f954438
vinicius-schulz <vinicius-schulz@hotmail.com>
2021-10-20 22:08:23 -0300

    Bug Adicionad

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
 
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
 @app.route("/soma")
 def soma_valores():
     x=10+10
