476c218a952921986d02c629c73c06d737188835
vinicius-schulz <vinicius-schulz@hotmail.com>
2021-10-20 22:16:38 -0300

    Bug Removido

--- app.py
+++ app.py
@@ -7,16 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+    return "Laboratório Pipeline DevOps"   
 
 @app.route("/soma")
 def soma_valores():
